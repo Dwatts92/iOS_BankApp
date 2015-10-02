e0401b37a68aac1e23020ba44803596ac6919ab4
Dwatts92 <ddwatts@asu.edu>
2015-10-01 18:02:23 -0700

    Bank App, local persistent storage - Fall 2014

---
+++ Assignment1prac/PasswordViewController.h
@@ -0,0 +1,15 @@
+//
+//  PasswordViewController.h
+//  Assignment3 Dylan Watts (Assignment1.prac)
+//
+//  Created by ddwatts on 9/24/14.
+//  Copyright (c) 2014 Dylan. All rights reserved.
+//  Header for password view.
+
+#import <UIKit/UIKit.h>
+
+@interface PasswordViewController : UIViewController
+
+-(IBAction)handleSwipe:(UISwipeGestureRecognizer *)recognizer;      //swipe recognizer
+
+@end
